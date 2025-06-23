796fbc9d57edf87b90b5695c75b6090349c81791
ntn2142 <ntn2142@proton.me>
2025-06-23 16:35:00 +0200

    default nixos config

---
+++ NixOs/default.nix
@@ -0,0 +1,126 @@
+{ config, pkgs, ... }:
+let
+  systemVersion = "25.05";
+in
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # TODO: change this
+  networking.hostName = "default";
+
+  # Enable networking
+  # networking.networkmanager.enable = true;
+  # networking.networkmanager.wifi.backend = "wpa_supplicant";
+
+  # bluetooth
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    autoNumlock = true;
+  };
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ntn2142 = {
+    isNormalUser = true;
+    description = "ntn2142";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = false;
+  services.displayManager.autoLogin.user = "ntn2142";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    neovim # editor
+    gcc
+    rustup
+    git
+    ripgrep
+    fd
+    fzf
+    unzip
+    wget
+    nodejs_24
+    wayland-utils
+    wl-clipboard
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    direnv
+    nix-direnv
+    home-manager
+  ];
+
+  system.stateVersion = systemVersion;
+
+}
