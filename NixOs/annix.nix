2000a32d8abaa09e1d387c706decda33a7102f41
ntn2142 <ntn2142@proton.me>
2025-06-30 21:35:46 +0200

    annix: automatische drucker suche

--- NixOs/annix.nix
+++ NixOs/annix.nix
@@ -82,6 +82,11 @@ in
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
