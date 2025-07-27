05241847617b396fc1ac70b1c53a1fe1c388a67b
ntn2142 <ntn2142@proton.me>
2025-07-27 14:36:19 +0200

    cleanup programms

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -3,21 +3,14 @@
 
   # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
+    nh
     neovim # editor
-    gcc
-    rustup
     git
     ripgrep
     fd
     fzf
-    unzip
-    wget
-    nodejs_24
-    wayland-utils
-    wl-clipboard
     kdePackages.sddm-kcm # Configuration module for SDDM
-    direnv
-    nix-direnv
+    wl-clipboard
   ];
 
   programs = {
@@ -26,14 +19,6 @@
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
-    };
-
-    nh = {
-      enable = true;
-      clean = {
-        enable = true;
-        extraArgs = "--keep 3 --keep-since 3d";
-      };
     };
   };
 }
